5d225e46096f53adb65e2acafd6e56e0cd9c1df0
Rijakamran04 <rijakamran2003@gmail.com>
2024-08-11 21:01:22 +0500

    Level Design till 3

--- Assets/Script/Movement.cs
+++ Assets/Script/Movement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WolfMovement : MonoBehaviour
+public class Movement : MonoBehaviour
 {
     public float speed = 2.0f;
     public float forwardBound = 3.0f;  // Maximum Z value for forward movement
